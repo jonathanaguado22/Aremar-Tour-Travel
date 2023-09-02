6e8fa099ae89ad2f2404dda4fe318f90426ebb0d
Cesar Aguado <cesaraguado123@gmail.com>
2023-09-01 19:55:19 -0500

    react start

--- Client/my-react-app/src/App.tsx
+++ Client/my-react-app/src/App.tsx
@@ -1,24 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+interface prop {
+title: string
+}
+function App({title}: prop): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      Proyecto {title}
+      <Button variant="primary">Aremar</Button>
     </div>
   );
 }
